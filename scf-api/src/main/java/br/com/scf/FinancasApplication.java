440f57ecdef82227fd43b70d55e20f86719c6926
Henrique <henriquerdq@gmail.coim>
2022-10-20 17:47:50 -0300

    Inclusão cache e jenkins

--- scf-api/src/main/java/br/com/scf/FinancasApplication.java
+++ scf-api/src/main/java/br/com/scf/FinancasApplication.java
@@ -2,7 +2,9 @@ package br.com.scf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class FinancasApplication {
 
